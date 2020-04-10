c8d8100f0c6d13d27b1e541847413ce6e7f0ef36
rafaelfurtado <rafael.limafurtado@gmail.com>
2020-04-10 18:02:19 -0300

    versao inicial do mqttController

---
+++ main.py
@@ -0,0 +1,43 @@
+import paho.mqtt.client as mqtt
+import sys
+
+# definicoes:
+Broker = "192.168.0.112"
+PortaBroker = 1883
+KeepAliveBroker = 60
+TopicoSubscribe = "test"  # dica: troque o nome do topico por algo "unico",
+username = "seeds"
+password = "carro#7638"
+
+# Dessa maneira, ninguem ira saber seu topico de
+# subscribe e interferir em seus testes
+
+# Callback - conexao ao broker realizada
+def on_connect(client, userdata, flags, rc):
+    print("[STATUS] Conectado ao Broker. Resultado de conexao: " + str(rc))
+
+    # faz subscribe automatico no topico
+    client.subscribe(TopicoSubscribe)
+
+
+# Callback - mensagem recebida do broker
+def on_message(client, userdata, msg):
+    MensagemRecebida = str(msg.payload)
+
+    print("[MSG RECEBIDA] Topico: " + msg.topic + " / Mensagem: " + MensagemRecebida)
+
+
+# programa principal:
+try:
+    print("[STATUS] Inicializando MQTT...")
+    # inicializa MQTT:
+    client = mqtt.Client()
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(Broker, PortaBroker, KeepAliveBroker)
+    client.loop_forever()
+except KeyboardInterrupt:
+    print ("\nCtrl+C pressionado, encerrando aplicacao e saindo...")
+    sys.exit(0)
